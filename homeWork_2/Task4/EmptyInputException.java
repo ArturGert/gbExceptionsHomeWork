5ea02ea2906d0b88939e6251f456cce00aebf840
Artur_Gert <arturgert@andex.ru>
2023-10-02 00:26:03 +0300

    Add comment task 4

--- homeWork_2/Task4/EmptyInputException.java
+++ homeWork_2/Task4/EmptyInputException.java
@@ -1,6 +1,7 @@
 package gbExceptionHomeWork.homeWork_2.Task4;
-
+// класс исключения в случае, если вводится пустая строка
 public class EmptyInputException extends Exception {
+
     public EmptyInputException() {
         super("Введена пустая строка!");
     }
--- homeWork_2/Task4/UserDataInput.java
+++ homeWork_2/Task4/UserDataInput.java
@@ -7,12 +7,14 @@ package gbExceptionHomeWork.homeWork_2.Task4;
 import java.util.Scanner;
 
 public class UserDataInput {
+    // метод принимающий данные от пользователя
     public static String getInputData() {
         System.out.println("Введите строку: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
-
+    // метод, который проверяет введенные пользователем данные: методом .trim()  удаляются пробелы в начале и в конце строки, и если после удаления строка пустая,
+    // то бросается исключение, в обратном случае в консоль выводятся данные о том какая строка введена
     public static void checkEmptyInput(String inputData) throws  EmptyInputException{
         String trimmedinputData = inputData.trim();
         if (trimmedinputData.isEmpty()){
@@ -20,7 +22,7 @@ public class UserDataInput {
         }
         System.out.println("Введена строка: " + inputData);
     }
-
+    // основной метод с обработкой исключения ввода пустой строки
     public static void main(String[] args)  {
         try {
             String userInput = getInputData();
